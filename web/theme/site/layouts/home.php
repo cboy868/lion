b2b94a41882472451a5676b5f9240bd33ce80cc2
cboy <cboy868@163.com>
2017-01-03 09:17:37 +0800

    add FooTable compiled and update footable to FooTable

--- web/theme/site/layouts/home.php
+++ web/theme/site/layouts/home.php
@@ -124,40 +124,6 @@ HomeAsset::register($this);
 
                 </form>
             </div>
-
-          <!--   <div class="footer-info-container">
-            <div class="footer-info">
-                <div class="row nova-footer-info nova-mg-pd">
-                    <div class="col-md-3 nova-footer nova-footer-box1 collapsible mobile-collapsible">
-                        <h2 class="block-title">About Eleganza Tiles</h2>
-                        <div class="block-content" style="display: block;">
-                        <p><span>We are a leading brand of ceramic, porcelain and glass tiles in North America. Our extensive product line ranges from residential to commercial use and offers a wide array of possibilities that help homeowners, designers, architects, and builders alike bring their ideas to life.</span></p>
-                        <p class="p1">&nbsp;</p>
-                        <p class="view-more"><a href="#"><span>View More</span></a></p>
-                        </div>                          
-                    </div>
-                    <div class="col-md-5 nova-footer nova-footer-box2 collapsible mobile-collapsible">
-                                            <h2 class="block-title">Latest News&nbsp;</h2>
-                    <div class="block-content">
-                    <div class="row nova-mg-pd">
-                    <div class="col-md-5"><a href="#"><img title="Fence_new_products" src="/theme/site/static/img/fence_thumbnail.jpg" alt="Fence_new_products"></a></div>
-                    <div class="col-md-7">
-                    <h2 class="title">Exciting New Products&nbsp;</h2>
-                    <p class="post"><span>POSTED BY&nbsp;Eleganza Tiles - Anaheim</span>&nbsp;January 5, 2015</p>
-                    <p class="p1"><span class="s1">Start off the year with our newest product additions.&nbsp;</span>
-                    <a href="#">Click here</a> to visit our new product section.</p>
-                    </div>
-                    </div>
-                    </div>                  
-                    </div>
-
-                    <div class="col-md-3 nova-footer nova-footer-box3 collapsible mobile-collapsible">
-                        <h2 class="block-title">&nbsp;Instagram</h2>
-                        <p><a title="Instragram Link " href="#" target="_blank"><img src="/theme/site/static/img/Instagram_Thumnail_Tiles_Page_2.png" alt=""></a></p>
-                    </div>
-                </div>
-            </div>
-        </div>    -->
         <div class="footer-container">
             <div class="footer-aditional">
                 <div class="box-border-3 border-top pading-top-bottom-20">
@@ -219,21 +185,6 @@ HomeAsset::register($this);
                         </div>
                     </div>
                 
-                    <!-- <div class="col-md-3">
-                            <h4>Latest Tweets</h4>
-                        <div class="block-content" style="display: block;">
-                        <ul id="twitter_update_list">
-                            <div class="twitter-posts"></div>
-                        </ul>
-                        </div>                          
-                    </div>
-                    <div class="col-md-3">
-                                    <h4>FACEBOOK</h4>
-                        <div class="block-content" style="display: block;">
-                        <div id="fb-root"></div>
-                        <div class="fb-like-box" data-href="#" data-width="293" data-height="280" data-show-faces="true" data-colorscheme="dark" data-stream="false" data-header="true"></div>    
-                    </div> -->
-                                
                     </div>      
                 </div>
            </div>
