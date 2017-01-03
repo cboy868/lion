e707f1a5e05b57de2ea540585f0bf09f316b5767
cboy <cboy868@163.com>
2017-01-03 11:37:47 +0800

    update goods category

--- modules/shop/views/admin/goods/create.php
+++ modules/shop/views/admin/goods/create.php
@@ -34,6 +34,7 @@ $this->params['breadcrumbs'][] = $this->title;
 					'attrs'=>$attrs, 
 					'specs' => $specs,
 					'tables' => $tables,
+					'tags' => $tags,
 					'av' => []
 			    ]) ?>
 				<div class="hr hr-18 dotted hr-double"></div>
