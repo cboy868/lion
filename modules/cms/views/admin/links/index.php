7a0db5e7c3d6c0ea4597a1b7047791dabf5c6f7c
cboy <cboy868@163.com>
2017-01-03 10:13:11 +0800

    update Links to links view dir

---
+++ modules/cms/views/admin/links/index.php
@@ -0,0 +1,71 @@
+<?php
+
+use app\core\helpers\Html;
+use app\core\helpers\Url;
+use yii\widgets\Breadcrumbs;
+use app\core\widgets\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\modules\cms\models\LinksSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = '连接管理 可用于友情链接';
+$this->params['breadcrumbs'][] = $this->title;
+
+
+?>
+
+<div class="page-content">
+    <!-- /section:settings.box -->
+    <div class="page-content-area">
+        <div class="page-header">
+            <h1>
+            <!-- 
+                <?=  Html::a($this->title, ['index']) ?> 
+            -->
+                <small>
+                    <?=  Html::a('<i class="fa fa-plus"></i> 新增', ['create'], ['class' => 'btn btn-primary btn-sm new-menu']) ?>
+                </small>
+            </h1>
+        </div><!-- /.page-header -->
+
+        <div class="row">
+            <div class="col-xs-12">
+                <div class="search-box search-outline">
+                        <?php  echo $this->render('_search', ['model' => $searchModel]); ?>
+                </div>
+            </div>
+
+            <div class="col-xs-12 links-index">
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'tableOptions'=>['class'=>'table table-striped table-hover table-bordered table-condensed'],
+        // 'filterModel' => $searchModel,
+        'columns' => [
+            'id',
+            'name',
+            'link',
+            [
+                'label' => 'Logo',
+                'value' => function($data){
+                    $dir = dirname($data->logo);
+                    $file = basename($data->logo);
+
+                    return '<img src="'.$dir . '/100x36@' . $file . '" />';
+                },
+                'format' =>'raw'
+            ],
+            // 'intro:ntext',
+            // 'status',
+            // 'created_at',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+                <div class="hr hr-18 dotted hr-double"></div>
+            </div><!-- /.col -->
+        </div><!-- /.row -->
+    </div><!-- /.page-content-area -->
+</div>
