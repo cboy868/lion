7d9a492bc2624d4883b30df0f68b4554f9ba356c
cboy <cboy868@163.com>
2017-02-08 22:08:03 +0800

    update email send

---
+++ core/views/mail/contact.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>来自某某公司的邮件</title>
+</head>
+<body>
+	<p>您好，非常感谢您关注本公司产品，为方便您了解本公司产品信息，特为您提供本网站的登录账号，如半年内未登录，系统将会自动为您清除，请放心使用，具体信息如下:</p>
+    网址:<a href="http://www.lion.cn" target="_blank">lion</a>;
+    用户名:<?=$username?>;
+    密码:<?=$password?>;
+</body>
+</html>
