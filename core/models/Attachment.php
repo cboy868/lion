b5177b846e0bb58706c543d5260cb78f6ad044a4
cboy <cboy868@163.com>
2017-02-09 07:06:37 +0800

    cms

--- core/models/Attachment.php
+++ core/models/Attachment.php
@@ -159,7 +159,6 @@ class Attachment extends \yii\db\ActiveRecord
             if (!is_file($thumb_path)) {
                 if (is_file($srcFile)) {
                     $size = explode('x', str_replace('X', 'x', $size));
-                    p($size);
                     Image::autoThumb($srcFile, $thumb_path, $size);
                 } else {
                     return $default;
