b49a3e34942dc076cf685d804cc3fe49fc80ad11
cboy868 <cboy868@163.com>
2017-05-24 13:31:27 +0800

    wechat

--- modules/wechat/controllers/admin/UserController.php
+++ modules/wechat/controllers/admin/UserController.php
@@ -63,10 +63,19 @@ class UserController extends Controller
         $list = $users->data['openid'];
 
 
-
-        p($users );
-
         p($list);
+
+        $count = $users->count;
+        $total = $users->total;
+        $next = $users->next_openid;
+        if ($total > $count && $count!=0) {
+            $list = $userService->lists($next);
+            p($list);
+        }
+
+
+
+
 
         die;
     }
