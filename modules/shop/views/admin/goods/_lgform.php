bef65ef5ac52474ae6ff52953d1878bbf052f358
cboy868 <cboy868@163.com>
2017-06-04 20:31:37 +0800

    goods i18n

--- modules/shop/views/admin/goods/_lgform.php
+++ modules/shop/views/admin/goods/_lgform.php
@@ -1,10 +1,17 @@
-<?= $form->field($lg_model, "[$lg_model->language]name")->textInput(['maxlength' => true]) ?>
+<?= $form->field($lg_model, "[$lg_model->language]name")
+    ->textInput(['maxlength' => true])->hint('原文: '.$model->name) ?>
 
-<?= $form->field($lg_model, "[$lg_model->language]intro")->widget('app\core\widgets\Ueditor\Ueditor',['option' =>['res_name'=>'goods', 'use'=>'ue'] ]);?>
+<?= $form->field($lg_model, "[$lg_model->language]intro")
+    ->widget('app\core\widgets\Ueditor\Ueditor',['option' =>['res_name'=>'goods', 'use'=>'ue'] ]);?>
 
-<?= $form->field($lg_model, "[$lg_model->language]unit")->textInput(['maxlength' => true]) ?>
+<?= $form->field($lg_model, "[$lg_model->language]unit")
+    ->textInput(['maxlength' => true])
+    ->hint('原文: '.$model->unit) ?>
 
-<?= $form->field($lg_model, "[$lg_model->language]skill")->textArea(['maxlength' => true])->label('附加') ?>
+<?= $form->field($lg_model, "[$lg_model->language]skill")
+    ->textArea(['maxlength' => true])
+    ->label('附加')
+    ->hint('原文: '.$model->skill) ?>
 
 
 
