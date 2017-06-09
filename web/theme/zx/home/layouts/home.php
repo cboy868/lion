7e5d2d8b5860173656156c5199034efd3e16cfe8
cboy868 <cboy868@163.com>
2017-06-10 01:12:49 +0800

    baidu

--- web/theme/zx/home/layouts/home.php
+++ web/theme/zx/home/layouts/home.php
@@ -261,7 +261,20 @@ use yii\helpers\Url;
 
         <!-- Custom Scripts -->
         <script type="text/javascript" src="/theme/zx/static/js/custom.js"></script>
-
+        <script>
+            (function(){
+                var bp = document.createElement('script');
+                var curProtocol = window.location.protocol.split(':')[0];
+                if (curProtocol === 'https') {
+                    bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+                }
+                else {
+                    bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+                }
+                var s = document.getElementsByTagName("script")[0];
+                s.parentNode.insertBefore(bp, s);
+            })();
+        </script>
 
 <?php $this->endBody() ?>
 </body>
