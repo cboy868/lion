d97de691df0144644d1a4083b964ff0f8d5225e0
cboy868 <cboy868@163.com>
2017-06-10 19:49:19 +0800

    wechat menu

--- modules/wechat/controllers/admin/MenuController.php
+++ modules/wechat/controllers/admin/MenuController.php
@@ -286,7 +286,7 @@ class MenuController extends Controller
         ];
 
         if ($info = $menu->add($buttons, $matchRule)) {
-            p($info);die;
+            p($info['menuid']);die;
             Yii::$app->getSession()->setFlash('success', '同步微信菜单成功');
         } else {
             Yii::$app->getSession()->setFlash('error', '同步微信菜单失败，请重试或检查菜单是否符合规则');
