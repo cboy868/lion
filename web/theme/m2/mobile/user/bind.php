eff26f5920709503cfeb6eca2435c085bff2c06f
cboy868 <cboy868@163.com>
2017-06-23 00:29:44 +0800

    update wechat web

--- web/theme/m2/mobile/user/bind.php
+++ web/theme/m2/mobile/user/bind.php
@@ -2,7 +2,7 @@
 $this->title="绑定系统账号";
 
 ?>
-<div class="content">
+<div class="content" id="bind">
 
     <div class="page__bd">
         <div class="weui-panel weui-panel_access">
@@ -26,34 +26,36 @@ $this->title="绑定系统账号";
         </div>
     </div>
     <div class="weui-btn-area">
-        <a href="javascript:;" class="weui-btn weui-btn_primary" @click="save">确定提交</a>
+        <a href="javascript:;" class="weui-btn weui-btn_primary" @click="bind">确定提交</a>
     </div>
 
 </div>
 
-<?php $this->beginBlock('news') ?>
+<?php $this->beginBlock('bind') ?>
     var wechat_uid = "<?=$wechat['id']?>";
     var demo = new Vue({
-        el: '#news-box',
+        el: '#bind',
         data: {
-            wechat_uid:"<?=$wechat['id']?>",
+            wechat_uid:wechat_uid,
             uname:'',
             passwd:'',
-            apiBind: base_url + 'wechat-users/bind',
+            apiBind: base_url + 'wechat-users/bind'
         },
         methods: {
-            //if(!this.uname || !this.passwd){$.toptip("账号与密码均必填", 'error'); return;}
-            if(this.uname.length<2) {$.toptip("账号长度不可小于两位", 'error'); return;}
-            if(this.passwd.length<6) {$.toptip("密码长度不可小于六位", 'error'); return;}
-            var data = {wechat_uid:this.wechat_uid,uname:this.uname,passwd:this.passwd};
+            bind: function () {
+                //if(!this.uname || !this.passwd){$.toptip("账号与密码均必填", 'error'); return;}
+                if(this.uname.length<2) {$.toptip("账号长度不可小于两位", 'error'); return;}
+                if(this.passwd.length<6) {$.toptip("密码长度不可小于六位", 'error'); return;}
+                var data = {wechat_uid:this.wechat_uid,uname:this.uname,passwd:this.passwd};
 
-            this.$http.post(this.cartUrl, data,{emulateJSON:true}).then(function(response){
-                console.dir(response);
-            }, function(response){
+                this.$http.post(this.apiBind, data,{emulateJSON:true}).then(function(response){
+                    console.dir(response);
+                }, function(response){
 
-            });
+                });
 
+            },
         }
     })
 <?php $this->endBlock() ?>
-<?php $this->registerJs($this->blocks['news'], \yii\web\View::POS_END); ?>
+<?php $this->registerJs($this->blocks['bind'], \yii\web\View::POS_END); ?>
