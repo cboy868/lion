b8c6f8cb799bcaf7f80944a7121ed9db72485f30
cboy868 <cboy868@163.com>
2017-06-22 14:51:36 +0800

    wechat

--- modules/grave/controllers/m/DefaultController.php
+++ modules/grave/controllers/m/DefaultController.php
@@ -146,7 +146,7 @@ class DefaultController extends \app\core\web\MController
         $query = Tomb::find()->where(['user_id'=>Yii::$app->user->id]);
 
         if ($query->count() == 0) {
-            return $this->redirect(['/m/user','wid'=>Yii::$app->request->get('id')]);
+            return $this->redirect(['/m/user','wid'=>Yii::$app->request->get('wid')]);
         }
 
         if ($query->count() == 1) {
