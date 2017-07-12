95aefa527473e93d10d95b70968930b377357a91
cboy868 <cboy868@163.com>
2017-07-12 23:14:49 +0800

    api user info

--- modules/api/controllers/common/WechatProUserController.php
+++ modules/api/controllers/common/WechatProUserController.php
@@ -164,7 +164,6 @@ class WechatProUserController extends Controller
         $user = \app\modules\user\models\User::findOne($post['id']);
         $user->email = $post['email'];
         $user->mobile = $post['mobile'];
-        return $user;
         return $user->save();
     }
 
