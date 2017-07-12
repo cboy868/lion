24b030ccdf65d904088728c1a0967f16f6c70215
cboy868 <cboy868@163.com>
2017-07-12 11:20:08 +0800

    program

---
+++ modules/api/controllers/v1/WechatProUserController.php
@@ -0,0 +1,18 @@
+<?php
+namespace app\modules\api\controllers\v1;
+
+use Yii;
+use yii\rest\ActiveController;
+use yii\filters\auth\QueryParamAuth;
+use yii\helpers\ArrayHelper;
+use yii\web\Response;
+use yii\data\ActiveDataProvider;
+
+use app\modules\news\models\NewsPhoto;
+/**
+ * Site controller
+ */
+class WechatProUserController extends \app\modules\api\controllers\common\WechatProUserController
+{
+	
+}
