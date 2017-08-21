be4b053bfa7677ee9406db797f191fa8ab329373
wansq <cboy868@163.com>
2017-08-22 00:12:58 +0800

    wechat pay

--- modules/wechat/config.php
+++ modules/wechat/config.php
@@ -22,7 +22,7 @@ return [
             'key'                => 'ba8c8eef2ce4a75eb264485baabbf6ae',
             'cert_path'     => Yii::getAlias('@app/web/static/cert/apiclient_cert.pem'),//'path/to/your/cert.pem', // XXX: 绝对路径！！！！
             'key_path'      => Yii::getAlias('@app/web/static/cert/apiclient_key.pem'),//'path/to/your/key',
-            'notify_url'    => Url::toRoute('/memorial/home/hall/notify')
+            'notify_url'    => Url::toRoute('/memorial/home/hall/notify', true)
         ],
 
         'menu' => [
