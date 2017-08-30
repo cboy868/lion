d7cc8ed910faf785e7cab0bd18aa3793567ac571
wansq <cboy868@163.com>
2017-08-30 23:25:46 +0800

    task api

--- modules/api/models/common/Task.php
+++ modules/api/models/common/Task.php
@@ -24,4 +24,20 @@ class Task extends \app\modules\task\models\Task
         ];
     }
 
+    public function fields()
+    {
+
+        $fields = parent::fields();
+        $other = [
+            'add_date' => function($model){
+                return date('Y-m-d H:i', $model->created_at);
+            },
+        ];
+
+        $fields = array_merge($fields, $other);
+
+        return $fields;
+
+    }
+
 }
