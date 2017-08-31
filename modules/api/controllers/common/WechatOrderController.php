ef8a0900b5dc6c3b8ad2e036b174fb99db75afb9
wansq <cboy868@163.com>
2017-08-31 15:48:32 +0800

    mini pro pay

--- modules/api/controllers/common/WechatOrderController.php
+++ modules/api/controllers/common/WechatOrderController.php
@@ -72,6 +72,7 @@ class WechatOrderController extends Controller
 
         if ($result->return_code == 'SUCCESS' && $result->result_code == 'SUCCESS'){
 
+
             $response = [
                 'appId' => $options['app_id'],
                 'package' => 'prepay_id='.$result->prepay_id,
@@ -85,10 +86,10 @@ class WechatOrderController extends Controller
 
             $sign = strtoupper(call_user_func_array('MD5', [$sign_str]));
 
-//            return [
-//                'self' => $sign,
-//                'sign' => generate_sign($response, $options['payment']['key'])
-//            ];
+            return [
+                'self' => $sign,
+                'sign' => generate_sign($response, $options['payment']['key'])
+            ];
 ////
 //            $sign = strtoupper(call_user_func_array('MD5', [nt']['key'])]));
 //            return strtoupper(call_user_func_array($encryptMethod, [urldecode(http_build_query($attributes))]));
