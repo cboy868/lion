d9cacb778050137bc2d3d2b5937ce765dd8e0381
wansq <cboy868@163.com>
2017-08-31 11:41:35 +0800

    mini order api

--- modules/wechat/controllers/home/OrderController.php
+++ modules/wechat/controllers/home/OrderController.php
@@ -171,85 +171,85 @@ class OrderController extends \app\core\web\HomeController
     }
 
 
-    public function actionMiniOrder()
-    {
-        $id = Yii::$app->request->post('order_id');
-        $openId = Yii::$app->request->post('openid');
-
-        $order = Order::findOne($id);
-
-        if (!$order) {
-            return ['errno'=>1, 'error'=>'无此订单'];
-        }
-
-        if ($order->progress >= Order::PRO_PAY) {
-            return ['errno'=>1, 'error'=> '订单已支付'];
-        }
-
-        $rels = $order->rels;
-
-        $detail = '';
-        $body = $rels[0]->title;
-        foreach ($rels as $v) {
-            $detail .= $v->title. ',';
-        }
-
-        $pay = Pay::create($order);
-
-        $attr = [
-            'trade_type' => 'NATIVE',
-            'body' => $body . '等',
-            'detail' => $detail,
-            'out_trade_no'     => $pay->order_no,
-            'total_fee'        => $order->price * 100,
-            'openid'           => $openId
-        ];
-
-        $options = $this->getMiniOptions();
-
-
-        $app = new Application($options);
-
-        $order = new WechatOrder($attr);
-
-        $payment = $app->payment;
-
-        $result = $payment->prepare($order);
-
-        if ($result->return_code == 'SUCCESS' && $result->result_code == 'SUCCESS'){
-
-            return [
-                'appid' => $options['app_id'],
-                'prepayId' => $result->prepay_id,
-                'timeStamp' => time(),
-                'nonceStr' => uniqid(),
-            ];
-        }
-
-        return ['errno'=>1, 'error'=>'统一支付失败,请联系管理员'];
-    }
-
-    private function getMiniOptions()
-    {
-        $params  = Yii::$app->getModule('wechat')->params;
-
-        $options = [
-            'debug'  => $params['debug'],
-            'log' => $params['log'],
-            'app_id' => $params['miniProgram']['appid'],
-            'secret' => $params['miniProgram']['appsecret'],
-//            'token' => $params['wx']['token'],
-            'payment' => [
-                'merchant_id'        => $params['payment']['merchant_id'],
-                'key'                => $params['payment']['key'],
-                'cert_path'          => $params['payment']['cert_path'], // XXX: 绝对路径！！！！
-                'key_path'           => $params['payment']['key_path'],      // XXX: 绝对路径！！！！
-                'notify_url'         => $params['payment']['notify_url'],       // 你也可以在下单时单独设置来想覆盖它
-            ],
-        ];
-
-        return $options;
-    }
+//    public function actionMiniOrder()
+//    {
+//        $id = Yii::$app->request->post('order_id');
+//        $openId = Yii::$app->request->post('openid');
+//
+//        $order = Order::findOne($id);
+//
+//        if (!$order) {
+//            return ['errno'=>1, 'error'=>'无此订单'];
+//        }
+//
+//        if ($order->progress >= Order::PRO_PAY) {
+//            return ['errno'=>1, 'error'=> '订单已支付'];
+//        }
+//
+//        $rels = $order->rels;
+//
+//        $detail = '';
+//        $body = $rels[0]->title;
+//        foreach ($rels as $v) {
+//            $detail .= $v->title. ',';
+//        }
+//
+//        $pay = Pay::create($order);
+//
+//        $attr = [
+//            'trade_type' => 'NATIVE',
+//            'body' => $body . '等',
+//            'detail' => $detail,
+//            'out_trade_no'     => $pay->order_no,
+//            'total_fee'        => $order->price * 100,
+//            'openid'           => $openId
+//        ];
+//
+//        $options = $this->getMiniOptions();
+//
+//
+//        $app = new Application($options);
+//
+//        $order = new WechatOrder($attr);
+//
+//        $payment = $app->payment;
+//
+//        $result = $payment->prepare($order);
+//
+//        if ($result->return_code == 'SUCCESS' && $result->result_code == 'SUCCESS'){
+//
+//            return [
+//                'appid' => $options['app_id'],
+//                'prepayId' => $result->prepay_id,
+//                'timeStamp' => time(),
+//                'nonceStr' => uniqid(),
+//            ];
+//        }
+//
+//        return ['errno'=>1, 'error'=>'统一支付失败,请联系管理员'];
+//    }
+//
+//    private function getMiniOptions()
+//    {
+//        $params  = Yii::$app->getModule('wechat')->params;
+//
+//        $options = [
+//            'debug'  => $params['debug'],
+//            'log' => $params['log'],
+//            'app_id' => $params['miniProgram']['appid'],
+//            'secret' => $params['miniProgram']['appsecret'],
+////            'token' => $params['wx']['token'],
+//            'payment' => [
+//                'merchant_id'        => $params['payment']['merchant_id'],
+//                'key'                => $params['payment']['key'],
+//                'cert_path'          => $params['payment']['cert_path'], // XXX: 绝对路径！！！！
+//                'key_path'           => $params['payment']['key_path'],      // XXX: 绝对路径！！！！
+//                'notify_url'         => $params['payment']['notify_url'],       // 你也可以在下单时单独设置来想覆盖它
+//            ],
+//        ];
+//
+//        return $options;
+//    }
 
 
     protected function findTombModel($id)
