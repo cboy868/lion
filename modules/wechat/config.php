86162498a8c829c4630df90e5fb40d7643f3c668
wansq <cboy868@163.com>
2017-08-31 11:30:42 +0800

    mini pay

--- modules/wechat/config.php
+++ modules/wechat/config.php
@@ -17,6 +17,10 @@ return [
             'appsecret' => 'fa7aec57b599c0e1155b4ba19857a8f5',
             'token' => '7JPN8xArTFbvBgIjHXaDZdnwf3tQeY2c',
         ],
+        'miniProgram'=> [
+            'appid' => 'wxddc8ada25ff73f9e',
+            'appsecret' => 'f29ab7ad6001d56bc6b7f9e991afb3e5',
+        ],
         'payment' => [
             'merchant_id'        => '1487057712',
             'key'                => 'ba8c8eef2ce4a75eb264485baabbf6ae',
