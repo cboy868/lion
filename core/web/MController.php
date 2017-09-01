fd70915866e211a438aa689160f81e45f0429821
wansq <cboy868@163.com>
2017-09-01 15:24:54 +0800

    bug

--- core/web/MController.php
+++ core/web/MController.php
@@ -56,6 +56,8 @@ class MController extends \app\core\web\Controller
         $oauth = $this->app->oauth;
         $session = Yii::$app->getSession();
 
+        p($session);die;
+
         if (!$session->has('wechat.wechat_user')) {
 
             $session['target_url'] = Url::current();
