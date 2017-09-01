2444ffc499e78866b6729382228629e05254e4c2
wansq <cboy868@163.com>
2017-09-01 15:56:56 +0800

    bug

--- web/theme/m2/mobile/grave/tombs.php
+++ web/theme/m2/mobile/grave/tombs.php
@@ -16,36 +16,7 @@ $this->title="已购买的墓位";
 </div>
 
 <?php $this->beginBlock('memorial') ?>
-    var wid="<?=Yii::$app->request->get('wid')?>";
-    var uid = '<?=$wechat['user_id']?>';
-    $(function(){
-        var app = new Vue({
-            el:'#tomb-content',
-            data:{
-                tombs:[],
-                apiUrl:base_url +'tomb',
-                apiParams : {thumbSize:'50x50',uid:uid},
-            },
-            beforeMount: function() {
-                this.getList();
-            },
-            methods:{
-                getList(append=false){
-                    this.$http.jsonp(this.apiUrl,{'jsonp':'lcb', params:this.apiParams}).then(function (response) {
-                        if (response.body.errno) {
-                            $.toast.prototype.defaults.duration = 3000;
-                            $.toast(response.body.error, "error", function() {
-                                location.href="/m/user?wid=" + wid ;
-                            });
-                        }
-                        this.$set(this, 'tombs', response.data.items);
-                    }).catch(function () {
 
-                    });
-                }
-            }
-        });
-    })
 
 <?php $this->endBlock() ?>
 <?php $this->registerJs($this->blocks['memorial'], \yii\web\View::POS_END); ?>
