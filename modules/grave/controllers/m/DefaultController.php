0d801aad35a25a9c34d4fb59b05d631db6991be1
wansq <cboy868@163.com>
2017-09-01 16:03:12 +0800

    bug

--- modules/grave/controllers/m/DefaultController.php
+++ modules/grave/controllers/m/DefaultController.php
@@ -183,7 +183,7 @@ class DefaultController extends \app\core\web\MController
             return $this->redirect(['tomb', 'id'=>$model->id, 'wid'=>$this->wid]);
         }
 
-        return $this->render('tombs',[
+        return $this->render('ttt',[
             //'wechat' => ArrayHelper::toArray($this->wechat_user)
         ]);
     }
