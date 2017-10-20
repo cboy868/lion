4107f21c945f1b0d6eaa2b9017d898c90e5c957c
wansq <cboy868@163.com>
2017-10-20 11:37:23 +0800

    微信统计

--- modules/analysis/views/m/default/index.php
+++ modules/analysis/views/m/default/index.php
@@ -1,12 +1,85 @@
-<div class="analysis-default-index">
-    <h1><?= $this->context->action->uniqueId ?></h1>
-    <p>
-        This is the view content for action "<?= $this->context->action->id ?>".
-        The action belongs to the controller "<?= get_class($this->context) ?>"
-        in the "<?= $this->context->module->id ?>" module.
-    </p>
-    <p>
-        You may customize this page by editing the following file:<br>
-        <code><?= __FILE__ ?></code>
-    </p>
+<?php
+app\assets\EchartsAsset::register($this);
+
+$this->title = '数据统计';
+?>
+
+<div class="page-content">
+    <!-- /section:settings.box -->
+    <div class="page-content-area">
+        <div class="page-header">
+            <h1>数据统计</h1>
+        </div><!-- /.page-header -->
+
+        <h2>墓位销售情况统计</h2>
+        <div class="row">
+            <div class="col-xs-12 client-index">
+                <?=\app\modules\analysis\widgets\Analysis::widget(['name'=>'tombNum'])?>
+                <div class="hr hr-18 dotted hr-double"></div>
+            </div><!-- /.col -->
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 client-index">
+                <?=\app\modules\analysis\widgets\Analysis::widget(['name'=>'tombAmount'])?>
+                <div class="hr hr-18 dotted hr-double"></div>
+            </div><!-- /.col -->
+        </div><!-- /.row -->
+        <hr>
+
+        <h2>导购人员年份业务统计</h2>
+
+        <div class="row">
+            <div class="col-xs-12 client-index">
+                <?=\app\modules\analysis\widgets\Analysis::widget(['name'=>'guideYearPercent'])?>
+                <div class="hr hr-18 dotted hr-double"></div>
+            </div>
+            <div class="col-xs-12 client-index">
+                <?=\app\modules\analysis\widgets\Analysis::widget(['name'=>'guideYearCompare'])?>
+                <div class="hr hr-18 dotted hr-double"></div>
+            </div>
+        </div>
+        <hr>
+        <h2>
+            导购人员月份统计
+        </h2>
+        <div class="row">
+            <div class="col-xs-12 client-index">
+                <?=\app\modules\analysis\widgets\Analysis::widget(['name'=>'guideMonthPercent'])?>
+                <div class="hr hr-18 dotted hr-double"></div>
+            </div>
+            <div class="col-xs-12 client-index">
+                <?=\app\modules\analysis\widgets\Analysis::widget(['name'=>'guideMonthCompare'])?>
+                <div class="hr hr-18 dotted hr-double"></div>
+            </div>
+        </div>
+        <hr>
+        <h2>导购个人业务统计</h2>
+        <div class="row">
+            <div class="col-xs-12 client-index">
+                <?=\app\modules\analysis\widgets\Analysis::widget(['name'=>'guideSelfMonth'])?>
+                <div class="hr hr-18 dotted hr-double"></div>
+            </div><!-- /.col -->
+        </div><!-- /.row -->
+        <hr>
+
+        <h2>导购个人接待占比统计</h2>
+
+        <div class="row">
+            <div class="col-xs-12 client-index">
+                <?=\app\modules\analysis\widgets\Analysis::widget(['name'=>'clientYearPercent'])?>
+                <div class="hr hr-18 dotted hr-double"></div>
+            </div><!-- /.col -->
+        </div>
+        <hr>
+        <h2>导购个人接待对比统计</h2>
+        <div class="row">
+            <div class="col-xs-12 client-index">
+                <?=\app\modules\analysis\widgets\Analysis::widget(['name'=>'clientYearCompare'])?>
+                <div class="hr hr-18 dotted hr-double"></div>
+            </div><!-- /.col -->
+        </div><!-- /.row -->
+
+
+    </div><!-- /.page-content-area -->
 </div>
