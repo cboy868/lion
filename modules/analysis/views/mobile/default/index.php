313592b653ca05443e070e217aee40995faf4b96
wansq <cboy868@163.com>
2017-11-03 10:52:41 +0800

    mobile

--- modules/analysis/views/mobile/default/index.php
+++ modules/analysis/views/mobile/default/index.php
@@ -8,7 +8,22 @@ $this->title = '数据统计';
     <!-- /section:settings.box -->
     <div class="page-content-area" style="margin-bottom:50px;">
         <h2>墓位销售情况统计</h2>
-       
+        <div class="row">
+            <div class="col-xs-12 client-index">
+                <?=\app\modules\analysis\widgets\Analysis::widget(['name'=>'tombNum'])?>
+                <div class="hr hr-18 dotted hr-double"></div>
+            </div><!-- /.col -->
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 client-index">
+                <?=\app\modules\analysis\widgets\Analysis::widget(['name'=>'tombAmount'])?>
+                <div class="hr hr-18 dotted hr-double"></div>
+            </div><!-- /.col -->
+        </div><!-- /.row -->
+        <hr>
+
+        
 
 
     </div><!-- /.page-content-area -->
