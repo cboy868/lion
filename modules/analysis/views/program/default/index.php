2abca45f004ecb7ff47fb07e8a0baa7126fe28ff
wansq <cboy868@163.com>
2017-11-04 12:59:14 +0800

    mobile -> program

--- modules/analysis/views/program/default/index.php
+++ modules/analysis/views/program/default/index.php
@@ -8,7 +8,7 @@ $this->title = '数据统计';
     <!-- /section:settings.box -->
     <div class="page-content-area" style="margin-bottom:50px;">
         <h2>墓位销售情况统计</h2>
-
+        
 
 
     </div><!-- /.page-content-area -->
