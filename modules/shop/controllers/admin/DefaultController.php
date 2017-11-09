ab2e239b9b42a005cd7aa0641920a7962406eaaa
wansq <cboy868@163.com>
2017-11-09 10:23:46 +0800

    shop default index

--- modules/shop/controllers/admin/DefaultController.php
+++ modules/shop/controllers/admin/DefaultController.php
@@ -2,14 +2,14 @@
 
 namespace app\modules\shop\controllers\admin;
 
-
-use app\modules\analysis\models\SettlementRel;
-
 class DefaultController extends \app\core\web\BackController
 {
+    /**
+     * @return string
+     * @name 商品首页
+     */
     public function actionIndex()
     {
-
         return $this->render('index');
     }
 
